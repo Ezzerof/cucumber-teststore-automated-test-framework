a74344afd16623236083ed62495300c24256a0af
Ezzerof <christian.bitca@gmail.com>
2023-06-08 17:17:50 +0100

    Add elements for contact us page

--- src/test/java/teststore/chris/pom/ContactUsPage.java
+++ src/test/java/teststore/chris/pom/ContactUsPage.java
@@ -1,5 +1,13 @@
 package teststore.chris.pom;
 
-public class ContactUsPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 
+public class ContactUsPage {
+    private WebDriver webDriver;
+    private final By subjectSelector = new By.ByCssSelector("select[name='id_contact']");
+    private final By emailField = new By.ByCssSelector("input[placeholder='your@email.com']");
+    private final By messageField = new By.ByCssSelector("textarea[placeholder='How can we help?']");
+    private final By checkbox = new By.ByCssSelector("#psgdpr_consent_checkbox_1");
+    private final By sendButton = new By.ByCssSelector("input[value='Send']");
 }
