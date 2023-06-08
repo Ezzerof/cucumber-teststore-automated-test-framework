526729d73fd5f7c19c9d0a6a4432fdaa13d9ece4
Ezzerof <christian.bitca@gmail.com>
2023-06-08 16:10:32 +0100

    Initial commit

---
+++ src/main/java/teststore/chris/App.java
@@ -0,0 +1,13 @@
+package teststore.chris;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/teststore/chris/TestRunner.java
@@ -0,0 +1,15 @@
+package teststore.chris;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = {"src/test/resources/features"},
+        plugin = {"pretty", "html:target/testReport.html", "json:target/jsonReport.json", "rerun:target/rerun.txt"}
+//        tags = {""}
+)
+public class TestRunner {
+
+}
